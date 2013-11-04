82fcabc2b5288acf229f8ff32161ceecce9bc3dc
lramsey <lramsey177@gmail.com>
2013-11-04 14:25:45 -0800

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -18,11 +18,11 @@ RSpec::Matchers.define :have_no_error_message do
   end
 end
 
-def valid_signup
+def example_signup
   fill_in "Name",         with: "Example User"
   fill_in "Email",        with: "user@example.com"
   fill_in "Password",     with: "foobar"
-  fill_in "Confirmation", with: "foobar"
+  fill_in "Confirm Password", with: "foobar"
 end
 
 RSpec::Matchers.define :have_success_message do |message|
