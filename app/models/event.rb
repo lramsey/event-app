6b4008f4a7008b488c4400a71b65df64103c574e
lramsey <lramsey177@gmail.com>
2013-11-10 11:06:51 -0800

    fix deployment bug

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,9 @@
 class Event < ActiveRecord::Base
   belongs_to :user
   default_scope -> { order('created_at DESC') }
-  validates :details,     presence: true, length: { maximum: 50 }
-  validates :where,       presence: true, length: { maximum: 25}
-  validates_date :start,  presence: true, on_or_after: Date.current
-  validates_date :finish, presence: true, on_or_after: :start
-  validates :user_id,     presence: true
+  validates :details, presence: true, length: { maximum: 50 }
+  validates :where,   presence: true, length: { maximum: 25}
+  validates :start,   presence: true, date: { on_or_after: Date.current }
+  validates :finish,  presence: true, date: { on_or_after: :start }
+  validates :user_id, presence: true
 end
